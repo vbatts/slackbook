0f8d3039bc94693f7516f3011f0ffbc9cb2b66c6
Vincent Batts <vbatts@spiderpig.awarix.com>
2010-01-13 08:56:57 -0600

    updating the build script a smidgin'

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-xsltproc -xinclude -o book.html /usr/share/sgml/docbook/docbook-xsl-1.75.1/html/docbook.xsl main.xml
+#!/bin/sh
+STYLE_SHEET="/usr/share/xml/docbook/xsl-stylesheets-1.75.1/html/docbook.xsl"
+
+xsltproc \
+  -xinclude \
+  -o book.html \
+  $STYLE_SHEET \
+  main.xml
