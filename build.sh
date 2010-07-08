cb960440f0476588843f6a965f4c084c66749fc2
Vincent Batts <vbatts@hashbangbash.com>
2010-07-08 09:47:14 -0400

    adding the ident keyword

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# $Id$
+
 STYLE_SHEET="/usr/share/xml/docbook/xsl-stylesheets-1.75.1/html/docbook.xsl"
 if [ ! -r "$STYLE_SHEET" ] ; then
   STYLE_SHEET=$(find /usr/share/xml -name docbook.xsl | grep /html/)
